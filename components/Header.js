901dbf11d9c3d4a5e0b87b65ddcbf35dd0991ef2
chillpillftm <124795513+chillpillftm@users.noreply.github.com>
2023-02-08 15:15:52 +0100

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,8 @@
+const headerStyles = {
+  color: 'white',
+  background: 'black'
+}
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return <h1 className="title" style={headerStyles} >{title}</h1>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+
+const containerStyles = {
+  backgroundImage: `url(/matrix.jpg)`,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center',
+  height: '100vh'
+}
+
+const headerStyles = {
+  color: 'white',
+}
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container" style={containerStyles}>
+    {/* <div className="container" > */}
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Chill Pill</title>
+        <link rel="icon" href="/pixel_heart.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <Header title="In life, it's important to take 'Chill Pills' once in a while! " style={headerStyles}/>
+        <Image src="/blue_pill.png" alt="Chill Pill" width={300} height={500} />
+        <p className="description" style={headerStyles}>
+          Get $CHILL at beets.fi
         </p>
       </main>
 
